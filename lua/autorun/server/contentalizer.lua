4ea43a9ac3e9cd4571aea23e237219a7627536e2
Redox <mail@redox.to>
2024-07-02 19:47:26 +0200

    Style and replace dedi check

--- lua/autorun/server/contentalizer.lua
+++ lua/autorun/server/contentalizer.lua
@@ -1,8 +1,4 @@
--- By Billy
--- https://steamcommunity.com/sharedfiles/filedetails/?id=2716242595
--- https://github.com/WilliamVenner/contentalizer
-
-if CLIENT or not game.IsDedicated() then return end -- Just in case people subscribe to it (which they will)
+if game.SinglePlayer() then return end
 
 local exts = {
     vtf = true,
@@ -17,37 +13,35 @@ local exts = {
     png = true,
     properties = true,
 }
-local function findContent(path, dir)
-    local f, d = file.Find(path .. "*", dir)
-    if istable(f) then
-        for _, f in ipairs(f) do
-            local extension = string.GetExtensionFromFilename(f)
-            if extension and exts[extension] then
-                return true
-            end
+
+local function findContent( path, dir )
+    local f, d = file.Find( path .. "*", dir )
+    if istable( f ) then
+        for _, f in ipairs( f ) do
+            local extension = string.GetExtensionFromFilename( f )
+            if extension and exts[extension] then return true end
         end
     end
 
-    if istable(d) then
-        for _, d in ipairs(d) do
-            if path .. d ~= "lua" and findContent(path .. d .. "/", dir) then
-                return true
-            end
+    if istable( d ) then
+        for _, d in ipairs( d ) do
+            if path .. d ~= "lua" and findContent( path .. d .. "/", dir ) then return true end
         end
     end
     return false
 end
 
-print("[Contentalizer] Checking for content in addons...")
-for _, addon in SortedPairsByMemberValue(engine.GetAddons(), "title") do
+print( "[Contentalizer] Checking for content in addons..." )
+for _, addon in SortedPairsByMemberValue( engine.GetAddons(), "title" ) do
     if addon.wsid and addon.mounted then
-        local found = findContent("", addon.title)
+        local found = findContent( "", addon.title )
         if found then
-            local title = string.Replace(addon.title, "\n", " ")
-            local wsid = tostring(addon.wsid)
-            print("[Contentalizer] Adding " .. title .. " - " .. wsid)
-            resource.AddWorkshop(wsid)
+            local title = string.Replace( addon.title, "\n", " " )
+            local wsid = tostring( addon.wsid )
+            print( "[Contentalizer] Adding " .. title .. " - " .. wsid )
+            resource.AddWorkshop( wsid )
         end
     end
 end
-print("[Contentalizer] Done!")
+
+print( "[Contentalizer] Done!" )
